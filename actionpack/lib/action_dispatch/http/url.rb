7c438b2e7e89944478cfad7afa6ea64df986e2e6
thedarkone <thedarkone2@gmail.com>
2010-09-25 16:35:38 +0200

    Cache a few of Request's commonly called methods.

--- actionpack/lib/action_dispatch/http/url.rb
+++ actionpack/lib/action_dispatch/http/url.rb
@@ -15,12 +15,12 @@ module ActionDispatch
 
       # Returns 'https://' if this is an SSL request and 'http://' otherwise.
       def protocol
-        ssl? ? 'https://' : 'http://'
+        @protocol ||= ssl? ? 'https://' : 'http://'
       end
 
       # Is this an SSL request?
       def ssl?
-        @env['HTTPS'] == 'on' || @env['HTTP_X_FORWARDED_PROTO'] == 'https'
+        @ssl ||= @env['HTTPS'] == 'on' || @env['HTTP_X_FORWARDED_PROTO'] == 'https'
       end
 
       # Returns the \host for this request, such as "example.com".
@@ -40,7 +40,7 @@ module ActionDispatch
       # Returns a \host:\port string for this request, such as "example.com" or
       # "example.com:8080".
       def host_with_port
-        "#{host}#{port_string}"
+        @host_with_port ||= "#{host}#{port_string}"
       end
 
       # Returns the port number of this request as an integer.
